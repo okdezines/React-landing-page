8d67e355dcb4feec252b65b0d7491d81d242c612
ompa <nmaladina@hotmail.com>
2024-08-05 09:50:03 +1200

    add features section with icons

--- 24Folio/src/App.jsx
+++ 24Folio/src/App.jsx
@@ -1,3 +1,4 @@
+import FeatureSection from "./components/FeatureSection";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 
@@ -8,6 +9,7 @@ const App = () => {
         <Navbar />
         <div className="max-w-7xl mx-auto pt-20 px-6">
           <HeroSection />
+          <FeatureSection />
         </div>
       </div>
     </div>
