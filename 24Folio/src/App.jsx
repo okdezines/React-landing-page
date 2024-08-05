914d1e8eac1192a8e8574036943348c016cb58e2
ompa <nmaladina@hotmail.com>
2024-08-05 14:43:08 +1200

    added pricing file

--- 24Folio/src/App.jsx
+++ 24Folio/src/App.jsx
@@ -1,6 +1,8 @@
 import FeatureSection from "./components/FeatureSection";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import WorkFlow from "./components/WorkFlow";
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
         <div className="max-w-7xl mx-auto pt-20 px-6">
           <HeroSection />
           <FeatureSection />
+          <WorkFlow />
+          <Pricing />
         </div>
       </div>
     </div>
--- 24Folio/src/components/FeatureSection.jsx
+++ 24Folio/src/components/FeatureSection.jsx
@@ -4,7 +4,7 @@ const FeatureSection = () => {
   return (
     <div className="relative mt-20 border-b border-neutral-800 min-h-[800px]">
       <div className="text-center">
-        <span className="bg-neutral-800 text-orange-500 rounded-full h-6 text-sm font-medium px-2 py-1 uppercase">
+        <span className="bg-neutral-800 text-orange-500 rounded-full h-6 text-md font-medium px-6 py-1 uppercase">
           Feature
         </span>
         <h2 className="text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-20 tracking-wide">
@@ -19,11 +19,11 @@ const FeatureSection = () => {
         {features.map((feature, index) => (
           <div key={index} className="w-full sm:w-1/2 lg:w-1/3">
             <div className="flex">
-              <div className="flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-orange-700 justify-center items-center rounded-full">
+              <div className="flex mx-6 h-10 w-10 p-2 mt-6 bg-neutral-900 text-orange-700 justify-center items-center rounded-full">
                 {feature.icon}
               </div>
             </div>
-            <h5 className="mt-1 mb-6 text-xl">{feature.text}</h5>
+            <h5 className="mt-1 mb-4 text-xl">{feature.text}</h5>
             <p>{feature.description}</p>
           </div>
         ))}
